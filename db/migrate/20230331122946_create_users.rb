a669917acd472203f40c2fb389e8b5b60ec6410c
Last David Lantican <lastdavid.lantican@gmail.com>
2023-03-31 20:38:37 +0800

    add db tables: stocks, transactions, users
    + create db
    + add stocks migration
    + add transactions migration
    + add users migration

---
+++ db/migrate/20230331122946_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+
+      t.string :email, null: false
+      t.string :first_name
+      t.string :last_name
+      t.boolean :isadmin, default: false
+      t.string :status, default: "pending"
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,48 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_03_31_122946) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "company_name"
+    t.integer "price"
+    t.integer "market_cap"
+    t.integer "user_id"
+    t.string "symbol"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_stocks_on_user_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer "share"
+    t.integer "price"
+    t.decimal "value"
+    t.integer "stock_id"
+    t.string "transaction_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.boolean "isadmin", default: false
+    t.string "status", default: "pending"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
