059f8264a3fed08c72c1644e05afac5f6f97df79
robjrdev <rjyamsuan@gmail.comv>
2023-04-06 12:36:19 +0800

    Password confirmation  + add validation for password and password confirmation  + add field for password confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      redirect_to sign_in_path
+    if @user.valid?
+				@user.save
+				redirect_to sign_in_path
     else
       #flash.now[:notice] = @user.errors.full_messages.to_sentence
        render :new, status: :unprocessable_entity
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,42 @@
 class User < ApplicationRecord
 	include BCrypt
+	
+	attr_accessor :password_confirmation
 
 	has_many :stocks, dependent: :destroy
 	has_many :transactions
+  
 	validates :first_name, presence: true, length: { maximum: 20 }
 	validates :last_name, presence: true, length: { maximum: 20 }
 	validates :email, presence: true,
 					  format: { with: URI::MailTo::EMAIL_REGEXP },
 					  uniqueness: { case_sensitive: false }
 	validates :password, presence: true, length: { minimum: 5 }
-
+	validates :password_confirmation, presence: true, on: :create
+	validate :passwords_match
+	
+  
 	def password
-		@password
+	  @password
 	end
+  
 	def password=(raw)
-		@password = raw
-		self.password_digest = Password.create(raw)
+	  @password = raw
+	  self.password_digest = Password.create(raw)
 	end
-
+  
 	def is_password?(raw)
-		Password.new(password_digest).is_password?(raw)
+	  Password.new(password_digest).is_password?(raw)
 	end
 
-	def is_admin?
-		self.admin == true
+	def passwords_match
+		if password != password_confirmation
+		  errors.add(:password_confirmation, "does not match Password")
+		end
 	end
-end
+  
+	def is_admin?
+	  self.admin == true
+	end
+  end
+  
