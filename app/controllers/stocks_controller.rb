068c3f9e33fe1418d70a6688244b61bf2bf32293
Last David Lantican <lastdavid.lantican@gmail.com>
2023-04-07 18:47:42 +0800

    add wip search stock function
    + add view file for new stock (search)
    ! modified stocks_controller.rb for show and search method
    + add routes for respective stocks controller method

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,13 +1,45 @@
 class StocksController < ApplicationController
-    before_action :initialize_iex_client
+  before_action :initialize_iex_client
 
-    def index
-        @trending_stocks = @client.stock_market_list(:mostactive)
-    end
+  def index
+    @trending_stocks = @client.stock_market_list(:mostactive)
+  end
 
-    def show
-    end
+  def new
+    #
+  end
 
-    def search
+  def show
+    #
+    @stock = Stock.find_by(symbol: params[:symbol])
+    @user_stock = current_user.user_stocks.find_by(stock: @stock)
+    @shares = @user_stock.try(:quantity) || 0
+
+    begin
+      @quote = @client.quote(@stock.symbol)
+    rescue IEX::Errors::SymbolNotFoundError
+      flash[:danger] = 'Symbol not found. Please input a valid symbol.'
+      render :new
     end
+  end
+
+  def search
+    begin
+      @quote = @client.quote(params[:symbol])
+
+      @stock = Stock.find_by(symbol: params[:symbol])
+
+      if @stock.nil?
+        @stock = Stock.new
+        @stock.symbol = @quote.symbol
+        @stock.name = @quote.company_name
+        @stock.save
+      end
+
+      redirect_to stocks_show_path(@stock.symbol)
+    rescue IEX::Errors::SymbolNotFoundError
+      flash[:danger] = 'Symbol not found. Please input a valid symbol.'
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,31 @@
-Rails.application.routes.draw do
+Rails
+  .application
+  .routes
+  .draw do
+    scope '/dashboard' do
+      get '/stocks', to: 'stocks#index', as: :stocks_index
+      get '/stocks/search', to: 'stocks#new', as: :stocks_new
+      get '/stocks/:symbol', to: 'stocks#show', as: :stocks_show
+      post '/stocks', to: 'stocks#search', as: :stocks_search
+    end
 
+    get '/dashboard', to: 'pages#dashboard', as: :dashboard
+    get '/pending', to: 'pages#pending', as: :pending
+    get '/admin', to: 'pages#admin', as: :admin
 
-  scope '/dashboard' do
-    get '/stocks', to: 'stocks#index', as: :stocks_index
+    get 'sessions/new'
+    get '/signup' => 'users#new'
+    resources :users, only: [:create]
+
+    get '/sign_in' => 'sessions#new'
+    get '/sign_out' => 'sessions#destroy'
+    resources :sessions, only: %i[create destroy]
+
+    patch '/users/:id/update_status',
+          to: 'users#update_status',
+          as: 'update_user_status'
+    get '/users/:id/edit' => 'users#edit', :as => :edit_user
+    patch '/users/:id', to: 'users#update', as: :update_user
+
+    root to: 'sessions#new'
   end
-
-  get '/dashboard', to: 'pages#dashboard', as: :dashboard
-  get '/pending', to: 'pages#pending', as: :pending
-  get '/admin', to: 'pages#admin', as: :admin
-
-  get 'sessions/new'
-  get '/signup' => 'users#new'
-  resources :users, only: [:create]
-
-  get '/sign_in' => 'sessions#new'
-  get '/sign_out' => 'sessions#destroy'
-  resources :sessions, only: [:create, :destroy]
-
-  patch '/users/:id/update_status', to: 'users#update_status', as: 'update_user_status'
-  get '/users/:id/edit' => 'users#edit', as: :edit_user
-  patch '/users/:id', to: 'users#update', as: :update_user
-
-  root to: 'sessions#new'
-end
