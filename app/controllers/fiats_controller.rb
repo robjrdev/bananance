8e683774a5abfa5fbd6d213d89506ea46da6d69a
Last David Lantican <lastdavid.lantican@gmail.com>
2023-04-19 20:31:11 +0800

    fix errors and success messages

--- app/controllers/fiats_controller.rb
+++ app/controllers/fiats_controller.rb
@@ -1,5 +1,4 @@
 class FiatsController < ApplicationController
-
   def deposit
     @user = User.find(params[:id])
   end
@@ -13,16 +12,17 @@ class FiatsController < ApplicationController
     amount = params[:amount].to_i
 
     if amount <= 0
-      flash[:error] = "Amount must be greater than zero"
+      flash[:error] = ['Amount must be greater than zero']
       render :deposit, status: :unprocessable_entity
-		else
+    else
       if @user.update_attribute(:cash, @user.cash + amount)
-				fiat = Fiat.new(transaction_type: "Deposit", amount: amount, user: @user)
-				fiat.save
-        flash[:success] = "Deposit successful! You've deposited $#{amount}"
+        fiat =
+          Fiat.new(transaction_type: 'Deposit', amount: amount, user: @user)
+        fiat.save
+        flash[:success] = ["Deposit successful! You've deposited $#{amount}"]
         redirect_to dashboard_path
       else
-        flash[:error] = "Deposit failed"
+        flash[:error] = ['Deposit failed']
         render :deposit, status: :unprocessable_entity
       end
     end
@@ -33,22 +33,22 @@ class FiatsController < ApplicationController
     amount = params[:amount].to_i
 
     if amount <= 0
-      flash[:error] = "Amount must be greater than zero"
+      flash[:error] = ['Amount must be greater than zero']
       render :withdraw, status: :unprocessable_entity
     elsif amount > @user.cash
-      flash[:error] = "Insufficient funds"
+      flash[:error] = ['Insufficient funds']
       render :withdraw, status: :unprocessable_entity
     else
       if @user.update_attribute(:cash, @user.cash - amount)
-				fiat = Fiat.new(transaction_type: "Withdraw", amount: amount, user: @user)
-				fiat.save	
-        flash[:success] = "Withdraw successful! You've withdrawn $#{amount}"
+        fiat =
+          Fiat.new(transaction_type: 'Withdraw', amount: amount, user: @user)
+        fiat.save
+        flash[:success] = ["Withdraw successful! You've withdrawn $#{amount}"]
         redirect_to dashboard_path
       else
-        flash[:error] = "Withdraw failed"
+        flash[:error] = ['Withdraw failed']
         render :withdraw, status: :unprocessable_entity
       end
     end
   end
-
 end
