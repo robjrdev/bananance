b0798bec1c235a5fd4bd85eca2d8c84a41461794
Last David Lantican <lastdavid.lantican@gmail.com>
2023-04-29 11:15:22 +0800

    + add rspec test for iex_client_service

---
+++ spec/services/iex_client_service_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+RSpec.describe IexClientService, type: :service do
+  let(:iex_client_service) { IexClientService.new }
+  let(:iex_client) { instance_double('IEX::Api::Client') }
+
+  before do
+    allow(IEX::Api::Client).to receive(:new).and_return(iex_client)
+    allow(iex_client_service).to receive(:client).and_return(iex_client)
+  end
+
+  describe '#quote' do
+    let(:quote) { double('quote') }
+
+    before do
+      allow(iex_client).to receive(:quote).with('AAPL').and_return(quote)
+    end
+
+    it 'returns the quote for a given symbol' do
+      result = iex_client_service.quote('AAPL')
+      expect(result).to eq(quote)
+    end
+  end
+
+  describe '#stock_market_list' do
+    let(:market_type) { 'gainers' }
+    let(:market_list) { double('market_list') }
+
+    before do
+      allow(iex_client).to receive(:stock_market_list)
+        .with(market_type)
+        .and_return(market_list)
+    end
+
+    it 'returns the stock market list for a given market type' do
+      result = iex_client_service.stock_market_list(market_type)
+      expect(result).to eq(market_list)
+    end
+  end
+
+  describe '#chart' do
+    let(:chart) { double('chart') }
+
+    before do
+      allow(iex_client).to receive(:chart).with('AAPL').and_return(chart)
+    end
+
+    it 'returns the chart for a given symbol' do
+      result = iex_client_service.chart('AAPL')
+      expect(result).to eq(chart)
+    end
+  end
+
+  describe '#ref_data_symbols' do
+    let(:ref_data_symbols) { double('ref_data_symbols') }
+
+    before do
+      allow(iex_client).to receive(:ref_data_symbols).and_return(
+        ref_data_symbols,
+      )
+    end
+
+    it 'returns the reference data symbols' do
+      result = iex_client_service.ref_data_symbols
+      expect(result).to eq(ref_data_symbols)
+    end
+  end
+end
