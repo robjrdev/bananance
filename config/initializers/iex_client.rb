36208442c79e024ca56edebf4925e549802fb66f
robjrdev <rjyamsuan@gmail.comv>
2023-06-19 14:12:44 +0800

    + add production api key for iex

--- config/initializers/iex_client.rb
+++ config/initializers/iex_client.rb
@@ -1,4 +1,4 @@
 IEX::Api.configure do |config|
-  config.publishable_token = Rails.application.secrets.iex_api_key
+  config.publishable_token = Rails.application.credentials.iex_api_key
   config.endpoint = 'https://cloud.iexapis.com/v1'
 end
